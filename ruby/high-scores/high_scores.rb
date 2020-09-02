6d230c6973279d2fb74357bd5275495dde143c14
Kevin McGrevey <niv3kmcg@gmail.com>
2020-09-01 20:01:42 -0600

    Passes first High Scores test

--- ruby/high-scores/high_scores.rb
+++ ruby/high-scores/high_scores.rb
@@ -1,8 +1,8 @@
-=begin
-Write your code for the 'High Scores' exercise in this file. Make the tests in
-`high_scores_test.rb` pass.
+class HighScores
+  attr_reader :scores
+  
+  def initialize(scores)
+    @scores = scores
+  end
 
-To get started with TDD, see the `README.md` file in your
-`ruby/high-scores` directory.
-=end
-
+end
