1edfcca9217bc7fd9af97962d3cf9b243173150a
Kevin McGrevey <niv3kmcg@gmail.com>
2020-09-08 21:43:23 -0600

    Completes Series exercise

--- ruby/series/series.rb
+++ ruby/series/series.rb
@@ -1,14 +1,14 @@
 class Series
-  
   def initialize(given_list)
     @given_list = given_list
   end
 
   def slices(size)
+    raise ArgumentError if size > @given_list.length
     groups = []
     list = @given_list.split("")
-    list.each_cons(size) { |char| groups << char }
-    groups.flatten
+    list.each_cons(size) { |chars| groups << chars }
+    groups.map { |group| group.join }
   end
 end
 
