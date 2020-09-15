27b3c25aaba5230a545315de20900e5e9f738f40
Kevin McGrevey <niv3kmcg@gmail.com>
2020-09-15 12:28:31 -0600

    Completes Hamming exercise

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,4 +1,12 @@
 class Hamming
-
-
+  def self.compute(seq1, seq2)
+    counter = seq1.length
+    raise ArgumentError if counter != seq2.length
+    distance = 0
+    until counter == 0
+      distance += 1 if seq1[(counter - 1)] != seq2[(counter - 1)]
+      counter -= 1
+    end
+    distance
+  end
 end
