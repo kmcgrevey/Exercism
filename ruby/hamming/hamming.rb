8f2eae0b4ab72a5bdfffff681946c42d5b12c537
Kevin McGrevey <niv3kmcg@gmail.com>
2020-09-15 12:46:57 -0600

    Refactor Hamming to one-liner solution

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,12 +1,16 @@
 class Hamming
   def self.compute(seq1, seq2)
-    counter = seq1.length
-    raise ArgumentError if counter != seq2.length
-    distance = 0
-    until counter == 0
-      distance += 1 if seq1[(counter - 1)] != seq2[(counter - 1)]
-      counter -= 1
-    end
-    distance
+    raise ArgumentError if seq1.length != seq2.length
+    seq1.chars.zip(seq2.chars).count { |a, b| a != b }
   end
+  # def self.compute(seq1, seq2)
+  #   counter = seq1.length
+  #   raise ArgumentError if counter != seq2.length
+  #   distance = 0
+  #   until counter == 0
+  #     distance += 1 if seq1[(counter - 1)] != seq2[(counter - 1)]
+  #     counter -= 1
+  #   end
+  #   distance
+  # end
 end
