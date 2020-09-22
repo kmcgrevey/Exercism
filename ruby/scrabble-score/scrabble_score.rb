96fba2fc3a98a30ba34355c48c225671f64eb7ca
Kevin McGrevey <niv3kmcg@gmail.com>
2020-09-21 20:55:35 -0600

    Completes Scrabble Score exercise

--- ruby/scrabble-score/scrabble_score.rb
+++ ruby/scrabble-score/scrabble_score.rb
@@ -4,13 +4,42 @@ class Scrabble
   end
 
   def score
-    return 0 if @word == nil
-    ratings = { 'a' => 1,
-                'e' => 1 }
+    return 0 if @word == nil 
+    ratings = { 
+                'a' => 1,
+                'e' => 1, 
+                'i' => 1, 
+                'o' => 1, 
+                'u' => 1, 
+                'l' => 1, 
+                'n' => 1, 
+                'r' => 1, 
+                's' => 1, 
+                't' => 1, 
+                'd' => 2, 
+                'g' => 2, 
+                'b' => 3, 
+                'c' => 3, 
+                'm' => 3, 
+                'p' => 3, 
+                'f' => 4, 
+                'h' => 4, 
+                'v' => 4, 
+                'w' => 4, 
+                'y' => 4, 
+                'k' => 5, 
+                'j' => 8, 
+                'x' => 8, 
+                'q' => 10, 
+                'z' => 10 
+              }
     total_score = @word.chars.map do |letter|
-      ratings[letter] if ('a'..'z').include? letter
+      (('a'..'z').include? letter.downcase) ? ratings[letter.downcase] : 0
     end
     total_score.sum
   end
 
+  def self.score(word)
+    new(word).score
+  end
 end
