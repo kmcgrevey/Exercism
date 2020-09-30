3ee2bdfac0c3e919a84c764b00d86367ba92fc04
Kevin McGrevey <niv3kmcg@gmail.com>
2020-09-30 09:29:55 -0600

    Completes Matrix exercise

---
+++ ruby/matrix/matrix.rb
@@ -0,0 +1,21 @@
+class Matrix
+  def initialize(trix)
+    @trix = trix
+  end
+  
+  def rows
+    trix_rows = @trix.split("\n")
+    trix_rows.map do |row|
+      (row.split(" ")).map { |num| num.to_i }
+    end
+  end
+
+  def columns
+    trix_columns = []
+    new_rows = rows
+    (new_rows.first.length).times do
+      trix_columns << new_rows.map { |row| row.shift }
+    end
+    trix_columns
+  end
+end
