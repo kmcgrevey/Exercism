0d73b96628f8f381afcf91c714c4b192d4a23944
Kevin McGrevey <niv3kmcg@gmail.com>
2020-11-10 12:28:46 -0700

    Completes Clock exercise

--- ruby/clock/clock.rb
+++ ruby/clock/clock.rb
@@ -1,14 +1,49 @@
 class Clock
+  attr_accessor :minutes, :hours
+
   def initialize(time_parts)
     @hours = time_parts[:hour]
     @minutes = time_parts[:minute]
   end
   
   def make_time
-    Time.new(2001, 01, 01, @hours, @minutes)
+    make_hours
+    if @minutes && (@minutes >= 60 || @minutes.negative?)
+      min_hr = @minutes / 60
+      mins = @minutes - (min_hr * 60)
+      @hours += min_hr
+      # @hours >= 24 ? make_hours : @hours
+      @hours = make_hours
+      @minutes = mins
+    end
+
+    time = Time.new(2001, 01, 01, @hours, @minutes)
+    time.strftime("%H:%M")
+    # binding.pry
+  end
+
+  def make_hours
+    @hours ? (@hours = @hours % 24) : (@hours = 0)
   end
 
   def to_s
-    make_time.strftime("%H:%M")
+    # make_time.strftime("%H:%M")
+    make_time
+  end
+
+  def +(new_clock)
+    new_clock.make_hours
+    @minutes += new_clock.minutes
+    to_s
+  end
+ 
+  def -(new_clock)
+    new_clock.make_hours
+    @minutes -= new_clock.minutes
+    to_s
+  end
+
+  def ==(new_clock)
+    to_s == new_clock.to_s
   end
 end
