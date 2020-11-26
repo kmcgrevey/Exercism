e8fa06d063c0ae6f0c594898d6da9a394dc67c1e
Kevin McGrevey <niv3kmcg@gmail.com>
2020-11-25 18:25:40 -0700

    Refactors RNA exercise

--- ruby/rna-transcription/rna_transcription.rb
+++ ruby/rna-transcription/rna_transcription.rb
@@ -1,10 +1,5 @@
 class Complement
-  @dna_comp = {'G' => 'C',
-               'C' => 'G',
-               'T' => 'A',
-               'A' => 'U'}
-  
   def self.of_dna(nucl)
-      (nucl.chars.map { |char| @dna_comp[char] }).join
+      nucl.tr('GCTA', 'CGAU')
   end
 end
