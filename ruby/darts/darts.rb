9e52b40412ba2f8d9aee9f1f5278fcfd0e465240
Kevin McGrevey <niv3kmcg@gmail.com>
2021-04-13 15:22:33 -0600

    Completes Darts exercise

--- ruby/darts/darts.rb
+++ ruby/darts/darts.rb
@@ -6,7 +6,12 @@ class Darts
   end
 
   def score
-    0
+    radius = Math.sqrt(@xco**2 + @yco**2)
+    
+    return 0 if radius > 10
+    return 1 if radius <= 10 && radius > 5
+    return 5 if radius <= 5 && radius > 1
+    return 10 if radius <= 1
   end
 
 end
