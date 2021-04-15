ea2caef474bf380514f05e9c8dc4001b8bbd2cca
Kevin McGrevey <niv3kmcg@gmail.com>
2021-04-14 20:41:44 -0600

    Completes Prime Factors exercise

--- ruby/prime-factors/prime_factors.rb
+++ ruby/prime-factors/prime_factors.rb
@@ -1,7 +1,19 @@
 class PrimeFactors
+  require 'prime'
 
   def self.of(num)
-    []
+    primes = []
+    
+    Prime.each do |prime|
+      return primes if num == 1
+      
+      until num % prime != 0
+        num = num / prime
+        primes << prime
+      end
+    end
+     
+    primes
   end
 
 end
