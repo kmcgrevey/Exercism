444de50bc307de82889b87cd0763561973eadb29
Kevin McGrevey <niv3kmcg@gmail.com>
2021-04-28 10:48:05 -1000

    Cleaner solution to Roman Numerals exercise

--- ruby/roman-numerals/roman_numerals.rb
+++ ruby/roman-numerals/roman_numerals.rb
@@ -1,67 +1,31 @@
 class Integer
   ROMANS = {
-    1 => 'I',
-    4 => 'IV',
-    5 => 'V',
-    9 => 'IX',
-    10 => 'X',
-    40 => 'XL',
-    50 => 'L',
-    90 => 'XC',
-    100 => 'C',
-    400 => 'CD',
-    500 => 'D',
+    1000 => 'M',
     900 => 'CM',
-    1000 => 'M'
+    500 => 'D',
+    400 => 'CD',
+    100 => 'C',
+    90 => 'XC',
+    50 => 'L',
+    40 => 'XL',
+    10 => 'X',
+    9 => 'IX',
+    5 => 'V',
+    4 => 'IV',
+    1 => 'I'
   }
   
   def to_roman
     number = self
     numeral = ''
     
-    until number == 0
-      if number >= 1000
-        numeral << ROMANS[1000]
-        number -= 1000
-      elsif number >= 900
-        numeral << ROMANS[900]
-        number -= 900
-      elsif number >= 500
-        numeral << ROMANS[500]
-        number -= 500
-      elsif number >= 400
-        numeral << ROMANS[400]
-        number -= 400
-      elsif number >= 100
-        numeral << ROMANS[100]
-        number -= 100
-      elsif number >= 90
-        numeral << ROMANS[90]
-        number -= 90
-      elsif number >= 50
-        numeral << ROMANS[50]
-        number -= 50
-      elsif number >= 40
-        numeral << ROMANS[40]
-        number -= 40
-      elsif number >= 10
-        numeral << ROMANS[10]
-        number -= 10
-      elsif number == 9
-        numeral << ROMANS[9]
-        number -= 9
-      elsif number >= 5
-        numeral << ROMANS[5]
-        number -= 5
-      elsif number == 4
-        numeral << ROMANS[4]
-        number -= 4
-      elsif number < 4
-        numeral << ROMANS[1]
-        number -= 1
+    ROMANS.each do |arabic, roman|
+      while number >= arabic
+        numeral << roman
+        number -= arabic
       end
     end
-
+    
     numeral
   end
   
