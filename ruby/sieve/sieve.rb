53547cccbb6297323ab414e3168acdb703c28404
Kevin McGrevey <niv3kmcg@gmail.com>
2021-05-04 20:11:09 -0600

    Completes Sieve exercise

--- ruby/sieve/sieve.rb
+++ ruby/sieve/sieve.rb
@@ -1,11 +1,19 @@
 class Sieve
 
   def initialize(limit)
+    @range = (2..limit).to_a.unshift(nil,nil)
     @limit = limit
   end
 
   def primes
-    []
+    @range.each do |num|
+      next unless num #skips if nil
+      break if num*num > @limit
+      
+      (num*num).step(@limit, num) { |x| @range[x] = nil }
+    end
+
+    @range.compact
   end
 
 end
