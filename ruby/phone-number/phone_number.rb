c73ae0b64d6ce70ffa1ec0919a64751d1400de83
Kevin McGrevey <niv3kmcg@gmail.com>
2021-05-06 22:16:28 -0600

    Completes Phone Number exercise

--- ruby/phone-number/phone_number.rb
+++ ruby/phone-number/phone_number.rb
@@ -2,8 +2,16 @@ class PhoneNumber
 
   def self.clean(number)
     num = number.split('').find_all { |num| ('0'..'9').include?(num) }
-    # binding.pry
-    num.join
+    
+    if (num.size == 11) && (num.first == '1')
+      checkit(num.join[1..10])
+    elsif (num.size == 10)
+      checkit(num.join)
+    end
+  end
+
+  def self.checkit(phone)
+    phone if ('2'..'9').include?(phone[0]) && ('2'..'9').include?(phone[3])
   end
 
 end
