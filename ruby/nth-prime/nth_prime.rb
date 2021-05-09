668f904657a287c75e418ae7a9ebb8265af172b7
Kevin McGrevey <niv3kmcg@gmail.com>
2021-05-08 21:34:08 -0600

    Completes nth_prime exercise

--- ruby/nth-prime/nth_prime.rb
+++ ruby/nth-prime/nth_prime.rb
@@ -1,7 +1,17 @@
 class Prime
 
   def self.nth(target)
-    2
+    raise ArgumentError if target < 1
+
+    primes = []
+    number = 2
+    
+    until primes.size == target
+      primes << number if primes.none? { |prime| number % prime == 0 }
+      number += 1
+    end
+
+    primes.last
   end
 
 end
