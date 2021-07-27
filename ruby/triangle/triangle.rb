f519ba0331b424bf82b342094386d5f7379b5f61
Kevin McGrevey <niv3kmcg@gmail.com>
2021-07-26 22:44:28 -0600

    Completes Triangle exercise

--- ruby/triangle/triangle.rb
+++ ruby/triangle/triangle.rb
@@ -1,14 +1,29 @@
 class Triangle
 
   def initialize(sides)
-    # @side1 = sides[0]
-    # @side2 = sides[1]
-    # @side3 = sides[2]
     @sides = sides
   end
 
   def equilateral?
-    @sides.all? { |side| @sides[1] == side }
+    (counts.keys.count == 1) if @sides.all? { |side| side > 0 }
+  end
+
+  def isosceles?
+    counts.values.any? { |value| value >= 2 } && inequal_rule
+  end
+
+  def scalene?
+    counts.values.all? { |value| value == 1 } && inequal_rule
+  end
+
+  def counts
+    @sides.each_with_object(Hash.new(0)) { |num, counts| counts[num] += 1}
+  end
+
+  def inequal_rule
+    (@sides[0] + @sides[1] >= @sides[2]) &&
+    (@sides[0] + @sides[2] >= @sides[1]) && 
+    (@sides[1] + @sides[2] >= @sides[0])
   end
 
 end
